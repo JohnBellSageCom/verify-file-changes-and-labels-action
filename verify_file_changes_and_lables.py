772ccc0876ea0d71c5f3bb29880d51da3c4869c9
John Bell <john.bell@sage.com>
2021-03-18 10:05:43 +0000

    Refactor python file.

--- verify_file_changes_and_lables.py
+++ verify_file_changes_and_lables.py
@@ -7,6 +7,7 @@ from github import Github
 import typing
 from collections import namedtuple
 import fnmatch
+from functools import cached_property
 
 Arguments = namedtuple(
     'Arguments',
@@ -47,37 +48,71 @@ def get_env_var(env_var_name, echo_value=False) -> str:
     return value
 
 
-def filter_pr_reviews_to_bot(pr_review: PullRequestReview):
-    return ((pr_review.user.login == 'github-actions[bot]'
-             or 'There are changes to production translations in this pull request' in pr_review.body)
-            and pr_review.state == 'CHANGES_REQUESTED')
+class PrChecker:
+    def __init__(self, args: Arguments, pr: PullRequest):
+        self.pr = pr
+        self.args = args
 
+    @classmethod
+    def from_args(cls, args: Arguments):
+        repo = Github(args.token).get_repo(args.repo_name)
 
-def get_bots_pr_reviews(pr: PullRequest) -> PaginatedList[PullRequestReview]:
-    pr_reviews = pr.get_reviews()
-    return list(filter(filter_pr_reviews_to_bot, pr_reviews))
+        # Create a pull request object
+        pr = repo.get_pull(args.pr_number)
 
+        return cls(args, pr)
 
-def handle_pr_review(pr: PullRequest,
-                     critical_files_changed: bool,
-                     is_required_label_present: bool,
-                     args: Arguments):
-    bots_prs = get_bots_pr_reviews(pr)
+    @cached_property
+    def _pr_has_required_label(self) -> bool:
+        # Get the pull request labels
+        pr_labels = self.pr.get_labels()
 
-    if critical_files_changed and not is_required_label_present:
-        # If there were not valid labels, then create a pull request review, requesting changes
-        print(
-            f'This pull request contains critical changes and does not contain any of the valid labels: {args.valid_labels}')
-        if not len(bots_prs):
-            pr.create_review(body=f'{args.required_label_message}Please add one of the following labels: `{args.valid_labels}` to confirm '
-                             'these changes.',
-                             event='REQUEST_CHANGES')
-    else:
-        # If there were valid labels, dismiss the request for changes if present
-        for pr_review in bots_prs:
-            print('Dismissing changes request')
-            pr_review.dismiss(
-                args.label_added_message if is_required_label_present else args.changes_reverted_message)
+        # Check which of the label in the pull request, are in the
+        # list of valid labels
+        for label in pr_labels:
+            if label.name in self.args.valid_labels:
+                return True
+
+        return False
+
+    @cached_property
+    def _pr_has_changed_critical_files(self) -> bool:
+        pr_files = self.pr.get_files()
+        for changed_file in pr_files:
+            for pattern in self.args.file_globs:
+                if fnmatch.fnmatch(changed_file.filename, pattern):
+                    return True
+        return False
+
+    def verify_pr(self):
+        self._handle_pr_review()
+
+    def _filter_pr_reviews_to_bot(self, pr_review: PullRequestReview):
+        return ((pr_review.user.login == 'github-actions[bot]'
+                 or self.args.required_label_message in pr_review.body)
+                and pr_review.state == 'CHANGES_REQUESTED')
+
+    def _get_bots_pr_reviews(self) -> PaginatedList[PullRequestReview]:
+        pr_reviews = self.pr.get_reviews()
+        return list(filter(lambda review: self._filter_pr_reviews_to_bot(review), pr_reviews))
+
+    def _handle_pr_review(self):
+        bots_prs = self._get_bots_pr_reviews()
+
+        if self._pr_has_changed_critical_files and not self._pr_has_required_label:
+            # If there were not valid labels, then create a pull request review, requesting changes
+            print(
+                f'This pull request contains critical changes and does not contain any of the valid labels: {self.args.valid_labels}')
+            if not len(bots_prs):
+                pr.create_review(body=f'{self.args.required_label_message} Please add one of the following labels: `{self.args.valid_labels}` to confirm '
+                                 'these changes.',
+                                 event='REQUEST_CHANGES')
+        else:
+            # If there were valid labels, dismiss the request for changes if present
+            for pr_review in bots_prs:
+                print('Dismissing changes request')
+                pr_review.dismiss(
+                    self.args.label_added_message if self._pr_has_required_label else self.args.changes_reverted_message)
 
 
 def get_pr_reference(github_ref: str) -> int:
@@ -120,45 +155,10 @@ def get_args() -> Arguments:
                      required_label_message, label_added_message, changes_reverted_message)
 
 
-def pr_has_required_label(pr: PullRequest, valid_labels: str) -> bool:
-    # Get the pull request labels
-    pr_labels = pr.get_labels()
-    # This is a list of valid label found in the pull request
-    pr_valid_labels = []
-
-    # Check which of the label in the pull request, are in the
-    # list of valid labels
-    for label in pr_labels:
-        if label.name in valid_labels:
-            pr_valid_labels.append(label.name)
-
-    return len(pr_valid_labels) > 0
-
-
-def pr_has_changed_critical_files(pr: PullRequest, patterns: [str]) -> bool:
-    pr_files = pr.get_files()
-    for changed_file in pr_files:
-        for pattern in patterns:
-            if fnmatch.fnmatch(changed_file.filename, pattern):
-                return True
-    return False
-
-
 def main():
     args = get_args()
-
-    # Create a repository object, using the GitHub token
-    repo = Github(args.token).get_repo(args.repo_name)
-
-    # Create a pull request object
-    pr = repo.get_pull(args.pr_number)
-    critical_files_changed = pr_has_changed_critical_files(pr, args.file_globs)
-    is_required_label_present = pr_has_required_label(pr, args.valid_labels)
-
-    handle_pr_review(pr,
-                     critical_files_changed=critical_files_changed,
-                     is_required_label_present=is_required_label_present,
-                     args=args)
+    pr_checker = PrChecker.from_args(args)
+    pr_checker.verify_pr()
 
 
 if __name__ == '__main__':
